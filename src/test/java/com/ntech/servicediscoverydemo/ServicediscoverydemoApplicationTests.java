c6cc55ccb35eba39c778f7a8dc5504c8028be429
Nirav Barot <niravbarot@gmail.com>
2019-02-06 22:30:30 +1100

    first

---
+++ src/test/java/com/ntech/servicediscoverydemo/ServicediscoverydemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ntech.servicediscoverydemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServicediscoverydemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
